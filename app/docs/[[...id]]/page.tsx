b34820a2a3d48ccc2ce245f7d7e91c7806b8c647
James <jjguevarag@gmail.com>
2024-01-05 15:29:40 -0600

    .

--- app/docs/[[...id]]/page.tsx
+++ app/docs/[[...id]]/page.tsx
@@ -1,6 +1,8 @@
+import styles from "./style.module.css";
+
 const DocsPage = ({params}) => {
   console.log(params)
-  return <div>Docs</div>
+  return <div className={styles.title}>Docs</div>
 }
 
 export default DocsPage;
