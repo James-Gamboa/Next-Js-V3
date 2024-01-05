f4e8cd2268ca345128ff9e5217411fec82ca3124
James <jjguevarag@gmail.com>
2024-01-05 13:36:48 -0600

    .

---
+++ app/(dashboard)/todos/layout.jsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+const DashboardLayout = ({children}) => {
+  return  (
+    <div>
+    <h1>Dashboard</h1>
+    <div>{children}</div>
+    </div>
+)
+
+}
+
+export default DashboardLayout;
