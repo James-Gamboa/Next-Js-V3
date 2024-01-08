46f1c6e0432f345baffcfc48dbeaf5552664491a
James <jjguevarag@gmail.com>
2024-01-07 21:29:28 -0600

    .

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -1,7 +1,20 @@
-//@ts-nocheck
-const Todo = ({todo}) => {
-  return <div>{todo.content}</div>
+"use client"
+
+import { useTransition } from "react";
+import { completeTodo } from "../utils/actions";
+
+const Todo = ({ todo }) => {
+  const [isPending, startTransition] = useTransition()
+  return (
+    <div
+      className={`px-8 py-2 border border-black/25 cursor-pointer ${
+        todo.completed ? 'line-through text-black/30' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
+      {todo.content}
+    </div>
+  )
 }
 
-
-export default Todo ;
+export default Todo
